d89388d036cf9fc1799c17caff0cb6f688374553
yanhan <1204676580@qq.com>
2017-08-26 17:26:12 +0800

    增加redux

--- src/redux/actions/test.js
+++ src/redux/actions/test.js
@@ -1,9 +1,10 @@
 /**
  * Created by b on 2017/8/25.
  */
+export const ADD_TEXT = 'ADD_TEXT';
 export function addText(text) {
     return {
-        type: 'ADD_TEXT',
+        type: ADD_TEXT,
         data: text
     };
 }
