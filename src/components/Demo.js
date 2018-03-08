5dbd7182fee6ba013e378e78f87eb894b75e971d
yanhan <1204676580@qq.com>
2018-03-08 17:56:43 +0800

    增加ajax

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -5,18 +5,33 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
 import * as actions from '../redux/actions/test';
+import http from '../tools/ajax';
+
 
 class Demo extends React.Component{
+
     handleClick() {
 
         const { dispatch,  test} = this.props;
         dispatch(actions.addText(`文本${test.list.length + 1}`));
     }
+    handleClick1() {
+        let params = {
+            method: 'get',
+            url: 'http://127.0.0.1:8081/addUser',
+            data: {},
+        };
+        http.ajax(params).then(res=>{
+            console.log(res)
+        },(err)=>{
+            console.log(err)
+        })
+    }
     render(){
         console.log(this.props.test)
         return(
             <div>
-                <button onClick={()=>{this.handleClick()}}>增加文本</button>
+                <button onClick={()=>{this.handleClick1()}}>增加文本</button>
                 <div>
                     { this.props.test.list.map((item, index) => {
                         return <span className="result" key={index}>{item}</span>;
---
+++ src/tools/ajax.js
@@ -0,0 +1,33 @@
+/**
+ * Created by b on 2018/3/8.
+ */
+import axios from 'axios';
+
+const http = {
+    handelResponse(res) {
+        if(res.response){
+            if(res.response.status === 500){
+                this.showError('服务器返回异常');
+            }else{
+                this.showError(`请求失败：\n${res.message}`);
+            }
+        }else{
+            this.showError('网络错误，请求失败');
+        }
+    },
+    showError(msg){
+        console.log(msg);
+    },
+    ajax(params) {
+        params.headers = {'Content-Type': 'application/x-www-form-urlencoded'};
+        return new Promise((resolve,reject)=>{
+            axios(params).then((res)=>{
+                resolve(res.data)
+            },(res)=>{
+                reject(res);
+                this.handelResponse(res);
+            })
+        })
+    }
+};
+export default http;
